67d998ff7fd6c2bc69027373fbb36b02cc51df13
dimitarmitkov <55533699+dimitarmitkov@users.noreply.github.com>
2020-09-30 13:45:25 +0300

    Initial Commit

---
+++ MyFirstMvcApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Server.HTTP;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHttpServer server = new HttpServer();
+
+            server.AddRoute("/", HomePage);
+
+            server.AddRoute("/favicon.ico", Favicon);
+
+            server.AddRoute("/about", About);
+
+            server.AddRoute("/users/login", Login);
+
+            await server.StartAsync(63342);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse About(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse Login(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        static HttpResponse Favicon(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Server.HTTP/IHttpServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Server.HTTP
+{
+    public interface IHttpServer
+    {
+        void AddRoute(string path, Func<HttpRequest, HttpResponse> action);
+
+        Task StartAsync(int port);
+    }
+}
